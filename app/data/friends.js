fb766209c84ecafc5be956c7e337344779677974
redKlark <39846620+redKlark@users.noreply.github.com>
2018-11-30 12:16:29 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+	{
+		name: 'Ahmed',
+		photo:
+			'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg',
+		scores: [ '5', '1', '4', '4', '5', '1', '2', '5', '4', '1' ]
+	},
+	{
+		name: 'Arnold',
+		photo:
+			'https://ih0.redbubble.net/image.421466233.1085/poster%2C210x230%2Cf8f8f8-pad%2C210x230%2Cf8f8f8.lite-1.jpg',
+		scores: [ '4', '2', '5', '1', '3', '2', '2', '1', '3', '2' ]
+	},
+	{
+		name: 'Jeffrey Dahmer',
+		photo: 'https://www.biography.com/.image/t_share/MTE1ODA0OTcxNTk5NzU4ODYx/jeffrey-dahmer-9264755-1-402.jpg',
+		scores: [ '5', '2', '2', '2', '4', '1', '3', '2', '5', '5' ]
+	},
+	{
+		name: 'John Wick',
+		photo: 'https://icdn2.digitaltrends.com/image/shows-to-stream-john-wick-2.jpg',
+		scores: [ '3', '3', '4', '2', '2', '1', '3', '2', '2', '3' ]
+	},
+	{
+		name: 'Lou Reed',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Lou_Reed_1977.JPG/220px-Lou_Reed_1977.JPG',
+		scores: [ '4', '3', '4', '1', '5', '2', '5', '3', '1', '4' ]
+	},
+	{
+		name: 'Arthur Morgan',
+		photo:
+			'https://vignette.wikia.nocookie.net/rdr/images/f/fa/Arthur_Morgan-Gameplay.png/revision/latest?cb=20181019214018&path-prefix=es',
+		scores: [ '4', '4', '2', '3', '2', '2', '3', '2', '4', '5' ]
+	}
+];
+
+module.exports = friends;
